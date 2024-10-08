9cfdf950ec8e3840cab0e4256ebaa6f48d57e5e9
Намуунзул Баярсайхан <24LP2770@G401-CC0158.local>
2024-10-08 11:46:48 +0800

    neon holbow

--- front-end/src/app/page.jsx
+++ front-end/src/app/page.jsx
@@ -1,9 +1,15 @@
-"use client"
+"use client";
 
 import { Card } from "./components/Card";
 import { Header } from "./components/Header";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  const BACKEND_ENDPOINT = "http://localhost:8080/product";
+
+
   return (
     <div className="w-full flex justify-center flex-col">
       <div className="container">
